857ecd11ef74142a33362160d3cf60c2d58904cb
vinograd-j <vinograd1020gm@gmail.com>
2024-06-27 17:08:34 +0300

    initial commit

---
+++ src/test/java/net/vinograd/imageprocessingapi/ImageProcessingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vinograd.imageprocessingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
